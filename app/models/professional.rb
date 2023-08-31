0d47f8b5096843880ed0f40eff0a3e3e9e935ff0
sebastian medina <sebastianlopez_21@hotmail.com>
2023-08-31 09:50:36 -0400

    Will add model relationships

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,3 +1,5 @@
 class Professional < ApplicationRecord
-    
+    has_many :appointments
+    has_many :users, through: :appointments
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
+    has_many :appointments
+    has_many :professionals, through: :appointments
+
     validates :username, presence: true, uniqueness: true
     # validates :password, length: { minimum: 8 }
 end
