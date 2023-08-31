da182a644729672eea3bb1731b1830d109805138
sebastian medina <sebastianlopez_21@hotmail.com>
2023-08-30 22:53:24 -0400

    Will make changes toadd picture to professional migration

--- db/migrate/20230831025056_add_picture_to_professionals.rb
+++ db/migrate/20230831025056_add_picture_to_professionals.rb
@@ -1,4 +1,4 @@
-class AddPictureToProffesionals < ActiveRecord::Migration[6.1]
+class AddPictureToProfessionals < ActiveRecord::Migration[6.1]
   def change
     add_column :professionals, :picture, :string
   end
